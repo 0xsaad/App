062bfdcb85590ed5aa8b83da1b08ab24f9239aed
0xsaad <saad2002hassan@gmail.com>
2023-05-26 04:28:24 -0700

    Get Data

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-const multer = require("multer");
-const fs = require('fs');
 const PORT = 8000;
 
+const song = require("./SRC/routes/song");
+
+// mongoose.set("strictQuery", false);
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//     //cb(null, Date.now() + Path2D.extname(file.originalname))
+//   },
+// });
+
+//const upload = multer({ storage: storage });
+mongoose
+  .connect("mongodb://127.0.0.1:27017/App", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.use(express.json())
+app.use("/", song);
+app.listen(PORT, () => console.log(`Server Started at PORT:8000`));
 
 // Things to study before starting ye wala kaam ====> What? Why? how?
 // 1. JSON objects?
@@ -20,20 +42,24 @@ const PORT = 8000;
 // git remote add origin https://github.com/0xsaad/App.git
 // git push -u origin main
 
-app.get("/", (req, res) => {
-       // 1. 
-  });
+// app.get("/", (req, res) => {
+//        // 1.
+//   });
 
-app.post("/", (req,res) => {
-    // 1. Get keys from body. =========> body main jo elem, wo req main sy kesy get krny hain
-    // 2. Save them in DB. ========> database main save kesy krna h
-    /**
-     * response =====> {
-     *  message: "Song uploaded!"
-     *  data: songObj
-     * }
-     */
-})
-  
-  app.listen(PORT, () => console.log(`Server Started at PORT:8000`));
-  
+// app.post("/", (req,res) => {
+//         const Song1 = {
+//           Song: req.file.SongName,
+//           Description: req.file.SongDescription,
+//           Singer: req.file.SingerName,
+//           res.send(`${Song1}!`)
+//         };
+//         res.render("images", { images });
+//       });
+// 1. Get keys from body. =========> body main jo elem, wo req main sy kesy get krny hain
+// 2. Save them in DB. ========> database main save kesy krna h
+/**
+ * response =====> {
+ *  message: "Song uploaded!"
+ *  data: songObj
+ * }
+ */
